bba3252625f6d55ab8f03d2b45f943f5dc23d68e
ajayk1234 <ajaykangare1993@gmail.com>
2018-01-31 11:40:09 +0530

    Json Example using MVC

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,46 @@
 package controllers
 
 import javax.inject._
+
 import play.api._
+import play.api.data.Form
+import play.api.data.Forms.{mapping, nonEmptyText}
+import play.api.i18n.I18nSupport
 import play.api.mvc._
+import views.html.helper.form
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
-@Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+case class UserData(email: String, password: String)
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
+
+  val userDataForm: Form[UserData] = Form(
+    mapping(
+      "email" -> nonEmptyText,
+      "password" -> nonEmptyText
+  )(UserData.apply)(UserData.unapply)
+  )
+
+  def login = Action { implicit request =>
+    Ok(views.html.index(userDataForm))
+  }
+
+  def doLogin = Action { implicit request =>
+
+    val user = userDataForm.bindFromRequest.get
+
+
+    Ok(user.email)
+  }
+
+
+  def index = Action { implicit request =>
+    Ok("index")
   }
 }
+
